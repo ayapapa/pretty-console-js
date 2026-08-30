0a006ebefa43f45ac80cc6b5e5384455e7b6ecc8
ayapapa <ayapapajapan@yahoo.co.jp>
2026-08-31 07:18:07 +0900

    Modified package.json, vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,29 +1,29 @@
 import { defineConfig } from 'vite'
 import { configDefaults } from 'vitest/config'
 
-// TypeScriptやJSXのサポートが必要な場合は、pluginsに設定を追加
 export default defineConfig({
   build: {
     lib: {
-      entry: 'src/index.ts', // tsup の src/index.ts に合わせる
-      formats: ['es', 'cjs'], // --format esm,cjs に合わせる
+      entry: 'src/index.ts', // Match tsup's src/index.ts
+      formats: ['es', 'cjs'], // Match --format esm,cjs
       fileName: (format) => format === 'cjs' ? 'index.cjs' : 'index.js',
     },
-    outDir: 'dist', // --out-dir dist に合わせる
-    // 型定義 (--dts) の場合は、vite-plugin-dts が必要になるため、
-    // 追加でプラグイン設定を書く必要があります。
-  },  // テスト環境でのみ無効にする設定はここに書く
+    outDir: 'dist', // Match　--out-dir dist 
+    // For type definitions (--dts), `vite-plugin-dts` is required,
+    // so you need to add the plugin configuration.
+  },
+  // Write settings to disable only in the test environment here.
   test: {
-    // 除外するファイルパターン
+    // File patterns to exclude
     exclude: [...configDefaults.exclude, 'e2e/*'],
     
-    // カバレッジ計測の設定
+    // Coverage measurement settings
     coverage: {
       reporter: ['text', 'text-summary', 'json-summary', 'json', 'html'],
       include: ['src/**/*.{ts,tsx,js,jsx}'],
     },
     
-    // ルートディレクトリ
+    // Root directory
     root: '.',
   },
 })
